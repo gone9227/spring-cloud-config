3f98dd46ca39b2a7e8be1c96d79ca12fd0803261
gone9227 <gone9227@gmail.com>
2021-02-26 16:45:44 +0900

    config server 세팅

--- src/main/java/com/springconfig/test/TestApplication.java
+++ src/main/java/com/springconfig/test/TestApplication.java
@@ -1,13 +1,17 @@
 package com.springconfig.test;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.ConfigServerApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
